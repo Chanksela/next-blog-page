8aaa09b0ff2d08ca7c16a85cd4955ece04df92e5
chanksela <kakhachankseliani@gmail.com>
2024-10-31 11:10:50 +0400

    chore

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export default function Navbar() {
+	return (
+		<header className="bg-red-200 px-10 py-4">
+			<nav className="flex">
+				<p className="w-1/3">logo</p>
+				<ul className="flex justify-end w-full gap-5">
+					<Link href="/">Home</Link>
+					<Link href="/about">About</Link>
+					<Link href="/add-article">Add</Link>
+				</ul>
+			</nav>
+		</header>
+	);
+}
